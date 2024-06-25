d4926e5c41bb573a587ebfe0bba8fc0bd8687af9
Rodrigo Fassi <rodrifassi7@gmail.com>
2024-06-25 18:40:06 -0300

    fixed light theme issues

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,19 @@
+import animations from '@midudev/tailwind-animations';
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				velde:'#6bff66',
-				
-			}
-		},
-		boxShadow: {
-			green: '0 4px 6px -1px rgba(107, 255, 102, 0.1), 0 2px 4px -1px rgba(107, 255, 102, 0.06)', // Definir la sombra verde personalizada
-		  },
-	},
-	plugins: [require("daisyui"),
-		require('tailwindcss-animated')
-	  ],
-}
-
-
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        velde: '#6bff66',
+      },
+      boxShadow: {
+        green: '0 4px 6px -1px rgba(107, 255, 102, 0.1), 0 2px 4px -1px rgba(107, 255, 102, 0.06)', // Definir la sombra verde personalizada
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    animations,
+  ],
+};
